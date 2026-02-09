f15489ede08f0823e287ac291a2f3b7eca420ae9
themichellesarmiento <michellechicsarmiento@yahoo.com>
2026-02-09 14:27:31 +0100

    add fallback route

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ app.use(express.static('public'));
 
 app.set('view engine',"ejs")
 
+/*routes*/
 app.use('/' ,homepageRoute);
 app.use('/mammals' ,mammalsRoute);
 app.use('/reptiles' ,reptilesRoute);
 app.use('/birds',birdsRoute);
 app.use('/aboutus' ,aboutusRoute);
+
+/*404 fallback */
+app.use((req, res) => {
+  res.status(404).send(`
+    <h2>Page Not Found!</h2>
+    <p>The page ${req.originalUrl} does not exist </p>
+    <a href="/">Back to Homepage</a>
+  `)
+})
 
 const port = 8647;
 app.listen(port, () => (
