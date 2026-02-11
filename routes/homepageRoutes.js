3a9fe83e8d621c79b2e108f55736133c54bcb2a2
claudiacarion <claudia.carion@gmail.com>
2026-02-11 15:26:19 +0100

    redeclare groupArray in homepageRoutes

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -1,31 +1,28 @@
-import express from 'express';
+import express from "express";
 import * as path from "path";
-import { birdsArray, mammalsArray, reptilesArray } from '../data/data.js';
+import { birdsArray, mammalsArray, reptilesArray } from "../data/data.js";
 
 const router = express.Router();
 const __dirname = path.resolve();
 
+router.get("/", (req, res) => {
+  res.render(path.join(__dirname, "/views/pages/page"));
+});
 
-router.get('/',(req,res)=>{
-  res.render(path.join(__dirname, "/views/pages/page"), {
-    groupArray: [birdsArray, mammalsArray, reptilesArray]
-  })
-})
-
-router.get('/:slug', (req, res) => {
+router.get("/:slug", (req, res) => {
   const { slug } = req.params;
+  const groupArray = [birdsArray, mammalsArray, reptilesArray];
 
-  const selectedAnimal = groupArray.forEach(group => group.forEach(animal => animal.find(animal.slug === slug)))
+  const selectedAnimal = groupArray.forEach(group => group.forEach(animal => animal.find(animal.slug === slug)));
 
   if (!selectedAnimal) {
     return res.status(404).send("Animal not found");
   }
 
-  res.render(path.join(__dirname, "/views/pages/animals"),
-  {
+  res.render(path.join(__dirname, "/views/pages/animals"), {
     allAnimals: "Zoo",
-    homeAnimal: [selectedAnimal]
-  })
-})
+    homeAnimal: [selectedAnimal],
+  });
+});
 
 export default router;
