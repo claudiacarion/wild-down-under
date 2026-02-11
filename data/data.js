31117c4aa520e31c4b5040ec8b951b8a8e47093e
claudiacarion <claudia.carion@gmail.com>
2026-02-11 14:51:43 +0100

    add subroutes and partial for animals on the home sidebar

--- data/data.js
+++ data/data.js
@@ -5,7 +5,7 @@ export const birdsArray = [
     description:
       "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida",
     diet: "Plants matter like fruit, insects and small animals like mice amd lizards.",
-    group: "Bird",
+    group: "Birds",
     image: "../images/birds/cassowary.png",
     location: "Queensland",
     lifespan: "20 years",
@@ -18,7 +18,7 @@ export const birdsArray = [
     description:
       "Native to the eucalyptus forests of Eastern Australia, the laughing Kookaburra is the largest member of the Kingfisher family, with females weighing up to one pound and growing up to 43cm in length. It's beak can reach 10cm long and is used to snatch a variety of inverebrates and small vertebrates, including the occasional small snake. Since being introduced into Western Australia and New Zealand, the Kookaburra has angered farmers by pretying on their fowl. The laughing Kookaburra has dark brown wing plumage and a white head and underside, Dark brown eye stripes run across it's face and it's upper bill is black. It's reddish-coloured tail is patterend with black bars.",
     diet: "Insects and small animals including snakes, frogs and lizards.",
-    group: "Bird",
+    group: "Birds",
     image: "../images/birds/kookaburra.png",
     location: "Australia wide",
     lifespan: "20 years",
@@ -31,7 +31,7 @@ export const birdsArray = [
     description:
       "It has a short crest on the top of it's head. It's plumage is mostly brownish black and it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red eye-rings, and the female has a bone-coloured beak and grey eye-rings. In flight, Yellow-tailed Black Cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. Their loud, wailing calls carry for long distances. The whiteae is found south of Victoria and to the east of South Australia and is smaller in size. The Yellow-tailed Black Cockatoo is found in the temperate forests",
     diet: "Fruit, seeds and other plant material",
-    group: "Bird",
+    group: "Birds",
     image: "../images/birds/blackCockatoo.png",
     location: "SE Australia",
     lifespan: "41 years",
@@ -47,7 +47,7 @@ export const mammalsArray = [
     description:
       "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
     diet: "Insects such as ants and termites, bettles, larvae and worms.",
-    group: "Mammal",
+    group: "Mammals",
     image: "../images/mammals/echidna.png",
     location: "Throughout Australia",
     lifespan: "50 years",
@@ -60,7 +60,7 @@ export const mammalsArray = [
     description:
       "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.",
     diet: "Plant eaters, they munch on shrubs and grasses.",
-    group: "Mammal",
+    group: "Mammals",
     image: "../images/mammals/quokka.png",
     location: "Only found on Rottnest Island and a few places on mainland Western Australia..",
     lifespan: "10 years",
@@ -73,7 +73,7 @@ export const mammalsArray = [
     description:
       "The Tasmanian devil (Sarcophilus harrisii) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.",
     diet: "A predator, then eat meat from other animals such as wallabies and wombats.",
-    group: "Mammal",
+    group: "Mammals",
     image: "../images/mammals/tasmanian_devil.png",
     location: "Tasmania",
     lifespan: "5 years",
--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -1,12 +1,33 @@
 import express from 'express';
 import * as path from "path";
+import { birdsArray } from '../data/data.js';
+import { mammalsArray } from '../data/data.js';
+import { reptilesArray } from '../data/data.js';
 
 const router = express.Router();
 const __dirname = path.resolve();
 
 
 router.get('/',(req,res)=>{
-  res.render(path.join(__dirname, "/views/pages/page"))
+  res.render(path.join(__dirname, "/views/pages/page"), {
+    groupArray: [birdsArray, mammalsArray, reptilesArray]
+  })
+})
+
+router.get('/:slug', (req, res) => {
+  const { slug } = req.params;
+
+  const selectedAnimal = groupArray.forEach(group => group.forEach(animal => animal.find(animal.slug === slug)))
+
+  if (!selectedAnimal) {
+    return res.status(404).send("Animal not found");
+  }
+
+  res.render(path.join(__dirname, "/views/pages/animals"),
+  {
+    allAnimals: "Zoo",
+    homeAnimal: [selectedAnimal]
+  })
 })
 
 export default router;
