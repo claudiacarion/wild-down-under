ea9f330ca016a0df92c9519fe9c84ad87dc93e6b
themichellesarmiento <michellechicsarmiento@yahoo.com>
2026-02-11 15:03:23 +0100

    refactor sidebar

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -5,12 +5,30 @@ import { mammalsArray, reptilesArray, birdsArray } from '../data/data.js';
 const router = express.Router();
 const __dirname = path.resolve();
 
+const allAnimals = [
+  ...mammalsArray,
+  ...reptilesArray,
+  ...birdsArray,
+];
+
+const animalsByGroup = [];
+
+allAnimals.forEach(animal => {
+  let group = animalsByGroup.find(g => g.name === animal.group);
+
+  if (!group) {
+    group = { name: animal.group, animals: [] };
+    animalsByGroup.push(group);
+  }
+
+  group.animals.push(animal);
+});
+
 router.get('/', (req, res) => {
   res.render(path.join(__dirname, "/views/pages/page"),
     {
-      mammals: mammalsArray,
-      reptiles: reptilesArray,
-      birds: birdsArray
+      allAnimalsArray: animalsByGroup
+
     })
 });
 
