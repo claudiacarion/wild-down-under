35e1fecf950d8bca37deab0c2b482f0bd04e734b
claudiacarion <claudia.carion@gmail.com>
2026-02-12 13:49:33 +0100

    merge in f/doc-title

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.use((req, res) => {
   `)
 })
 
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 1234;
 app.listen(port, () => (
   console.log(`Listening on port ${port}..`)
 ));
