afddd12f656f3696e7604120e18bad5cf64e85ad
dobin-sPortfolio <dobin7955gmail.com$ git config --global user.email dobin7955gmail.com>
2026-02-12 15:01:00 +0100

    add class name

--- data/data.js
+++ data/data.js
@@ -102,7 +102,7 @@ export const reptilesArray = [
     description:
       "The Hawksbill Sea Turtle gets its common name from the distinctive mouth, which resembles a bird’s beak. The shell, or carapace, is covered in large overlapping scutes, which are a distinctive brown/green/amber background with lighter brown streaks throughout. At the edge of the carapace they overlap in such a way as to form a serrated edge. The turtle has an elongated head and flippers which have two visible claws on the end. The flippers and head are covered in large green, brown or yellow scales. The average adult female weighs 50 kg and their carapace (shell) is approximately 80 cm in length.",
     diet: "Other animals (sponges & jellyfish) and sea plants.",
-    group: "Reptile",
+    group: "Reptiles",
     image: "../images/reptiles/hawksbill_turtle.png",
     location: "Tropical coasts of Queensland, Northern Territory and Western Australia.",
     lifespan: "50 years",
@@ -115,7 +115,7 @@ export const reptilesArray = [
     description:
       "The perentie (Varanus giganteus) is the largest monitor lizard or goanna native to Australia. It is one of the largest living lizards on earth, after the Komodo dragon, Asian water monitor, crocodile monitor, and intersecting by size with Nile monitor. Found west of the Great Dividing Range in the arid areas of Australia, it is rarely seen, because of its shyness and the remoteness of much of its range from human habitation. The species is considered to be a least-concern species according to the International Union for Conservation of Nature. Its status in many Aboriginal cultures is evident in the totemic relationships, and part of the Ngiṉṯaka dreaming, as well as bush tucker. It was a favoured food item among desert Aboriginal tribes, and the fat was used for medicinal and ceremonial purposes.",
     diet: "Carnivorous, they eat animals like kangaroos, rabbits, lizards and birds.",
-    group: "Reptile",
+    group: "Reptiles",
     image: "../images/reptiles/perentie.png",
     location: "Deserts of Australia.",
     lifespan: "20 years",
