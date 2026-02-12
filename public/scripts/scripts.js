e24839cf9dfdd199303c2a6ed83663d0cf583af7
dobin-sPortfolio <dobin7955gmail.com$ git config --global user.email dobin7955gmail.com>
2026-02-12 20:49:22 +0100

    add hamburger menu

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -10,5 +10,18 @@ arrowButton.addEventListener('click',()=>{
     sidebarContent.classList.add('open');
     arrowButton.classList.add('animate');
   }
+})
 
+const menuBtn = document.querySelector(`.menu-btn`);
+const nav = document.querySelector(`.nav`);
+
+menuBtn.addEventListener(`click`, () => {
+  nav.classList.toggle(`active`);
+  menuBtn.classList.toggle(`active`)
+
+  // if(nav.classList.contains(`active`)) {
+  //   document.body.style.overflow = `hidden`;
+  // } else {
+  //   document.body.style.overflow = "auto"; 
+  // }
 })
