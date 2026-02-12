f9ea511144a6feeb483f26471130f7684e645d9c
claudiacarion <claudia.carion@gmail.com>
2026-02-12 13:36:54 +0100

    add document title for all main routes

--- routes/aboutusRoutes.js
+++ routes/aboutusRoutes.js
@@ -1,37 +1,36 @@
-import express from 'express';
+import express from "express";
 import * as path from "path";
-import { teamArray } from '../data/data.js';
+import { teamArray } from "../data/data.js";
 
 const router = express.Router();
 const __dirname = path.resolve();
 
-router.get('/', (req, res) => {
-  res.render(path.join(__dirname, "/views/pages/page"),
-  {
+router.get("/", (req, res) => {
+  res.render(path.join(__dirname, "/views/pages/page"), {
     groupArray: teamArray,
     teamName: "Zoo Keepers",
-    groupDescription: 
-    "Meet the zoo keepers: we wrangle wild ideas, feed big dreams, and somehow keep the whole ecosystem thriving. No tranquilizers needed—just teamwork and snacks.",
-    teamArray:teamArray
-  })
+    groupDescription:
+      "Meet the zoo keepers: we wrangle wild ideas, feed big dreams, and somehow keep the whole ecosystem thriving. No tranquilizers needed—just teamwork and snacks.",
+    teamArray: teamArray,
+    docTitle: "Wild Down Under | About Us",
+  });
 });
 
-router.get('/:slug', (req, res) => {
+router.get("/:slug", (req, res) => {
   const { slug } = req.params;
 
   const selectedMember = teamArray.find(team => team.slug === slug);
-  
+
   if (!selectedMember) {
     return res.status(404).send("Team member not found");
   }
 
-  res.render(path.join(__dirname, "/views/pages/animals"),
-  {
+  res.render(path.join(__dirname, "/views/pages/animals"), {
     teamName: "Zoo Keepers",
     team: [selectedMember],
-    teamArray:teamArray
-
-  })
-})
+    teamArray: teamArray,
+    docTitle: "Wild Down Under | About Us",
+  });
+});
 
 export default router;
--- routes/birdsRoutes.js
+++ routes/birdsRoutes.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import * as path from 'path';
-import { birdsArray } from '../data/data.js';
+import express from "express";
+import * as path from "path";
+import { birdsArray } from "../data/data.js";
 
 const router = express.Router();
 const __dirname = path.resolve();
 
-router.get('/', (req, res) => {
-  res.render(path.join(__dirname, "/views/pages/page"),
-  {
+router.get("/", (req, res) => {
+  res.render(path.join(__dirname, "/views/pages/page"), {
     groupArray: birdsArray,
     groupName: "Birds",
     groupDescription: "Meet our feathered superstars — part punk rock, part comedy act, all Aussie attitude!",
-    animalArray: birdsArray
-  })
+    animalArray: birdsArray,
+    docTitle: "Wild Down Under | Birds",
+  });
 });
 
-router.get('/:slug', (req, res) => {
+router.get("/:slug", (req, res) => {
   const { slug } = req.params;
 
   const selectedAnimal = birdsArray.find(animal => animal.slug === slug);
@@ -24,13 +24,12 @@ router.get('/:slug', (req, res) => {
     return res.status(404).send("Animal not found");
   }
 
-  res.render(path.join(__dirname, "/views/pages/animals"),
-  {
+  res.render(path.join(__dirname, "/views/pages/animals"), {
     groupName: "Birds",
     animals: [selectedAnimal],
-    animalArray: birdsArray
-
-  })
-})
+    animalArray: birdsArray,
+    docTitle: "Wild Down Under | Birds",
+  });
+});
 
 export default router;
