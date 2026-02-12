7468024a4e98ba43a620573c6167e2216dc40bbc
dobin-sPortfolio <dobin7955gmail.com$ git config --global user.email dobin7955gmail.com>
2026-02-12 20:50:06 +0100

    forgot to remove port

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -18,10 +18,4 @@ const nav = document.querySelector(`.nav`);
 menuBtn.addEventListener(`click`, () => {
   nav.classList.toggle(`active`);
   menuBtn.classList.toggle(`active`)
-
-  // if(nav.classList.contains(`active`)) {
-  //   document.body.style.overflow = `hidden`;
-  // } else {
-  //   document.body.style.overflow = "auto"; 
-  // }
 })
